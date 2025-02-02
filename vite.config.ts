b8b16c4fe127655d2c09b7baa2f0e15b207de1e7
PiyushMandase <piyushmandase0110@gmail.com>
2025-02-03 01:18:51 +0530

    Updated title

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://github.com/PiyushMandase/BGMI_Points-table25',
+  base: '/BGMI_Points-table25/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
